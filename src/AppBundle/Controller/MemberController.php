c11fe5f084249b65eb6e2c1b83014d97873a45d1
Nicolas PIN (Silverhead) <pin.nicolas@free.fr>
2017-01-24 23:26:46 +0100

    Add member view template (not finish)

--- src/AppBundle/Controller/MemberController.php
+++ src/AppBundle/Controller/MemberController.php
@@ -24,19 +24,60 @@ class MemberController extends Controller
         return $this->render('member/memberEdit.html.twig');
     }
 
-    /**
-     * @Route("/member/fiche/{id}", name="member_fiche", requirements={"di": "\d+"})
-     */
-    public function ficheMemberAction(Request $request, $id)
-    {
-        return $this->render('member/memberEdit.html.twig');
-    }
+//    /**
+//     * @Route("/member/fiche/{id}", name="member_fiche", requirements={"di": "\d+"})
+//     */
+//    public function viewMemberAction(Request $request, $id)
+//    {
+//        return $this->render('member/memberEdit.html.twig');
+//    }
 
-    /***
+    /**
      * @Route("/members/view/{id}", name="member_view", requirements={"id": "\d+"});
      */
     public function viewMemberAction(Request $request, $id)
     {
-        return $this->render('member/memberView.html.twig');
+        $member = $this->getMemberInfos();
+        return $this->render('member/memberView.html.twig', [
+            'member' => $member
+        ]);
+    }
+
+
+    private function getMemberInfos()
+    {
+        return (object) [
+            'firstName' => 'John',
+            'lastName' => 'Doe',
+            'birthday' => \DateTime::createFromFormat('Y-m-d', '1981-05-07' ),
+            'avatar' => 'user.png',
+            'contactInformation' => (object) [
+                'country' => 'France',
+                'address' => '1 rue jean bonbeur',
+                'postalCode' => '84000',
+                'city' => 'Avignon',
+                'phone' => '0123456789',
+                'mobile' => '0623456789',
+            ],
+            'connection' => (object) [
+                'pseudo' => 'johnDoe',
+                'email' => 'johndoe@email.com',
+                'group' => 'Utilisateur',
+                'active' => 'Oui'
+            ],
+            'infosMember' => (object) [
+                'status' => 'Membre premium',
+                'subscription' => 'Abonnement 1 an premium',
+                'periodicityPayment' => '1 an',
+                'specialities' => [
+                    'Electronique',
+                    'Informatique',
+                    'Travail du bois',
+                ],
+                'profession' => 'Développeur',
+                'birthday' => \DateTime::createFromFormat('Y-m-d', '1981-05-07' ),
+                'lastSubscriptionDate' => \DateTime::createFromFormat('Y-m-d', '2016-05-07' )
+            ]
+        ];
     }
 }
