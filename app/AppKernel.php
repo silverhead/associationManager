ea7b0776768fe3ed3e2111d63a97c1f3bcde78c0
Nicolas PIN (Silverhead) <pin.nicolas@free.fr>
2017-04-29 22:09:36 +0200

    setup of app setting part (wip)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle(),
             new AppBundle\AppBundle(),
         ];
 
