9c4adbbfd4e547d7f8f1ccf51f7b668aedfc11f6
Nicolas PIN (Silverhead) <pin.nicolas@free.fr>
2017-05-08 23:32:55 +0200

    start of Page historical handler (wip)

---
+++ src/AppBundle/Handler/PageHistoricalHandler.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolaspin
+ * Date: 08/05/2017
+ * Time: 23:17
+ */
+
+namespace AppBundle\Handler;
+
+
+class PageHistoricalHandler
+{
+
+    private $name;
+
+    private $toRoute;
+
+    private $fromRoute;
+}
